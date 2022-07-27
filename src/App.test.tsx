f7198656c5f45ebbce60b078da11703aeb3d7312
João Pedro Resende <joao.resende.ext@dock.tech>
2022-07-27 15:42:13 -0300

    disable eslint on app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,24 +1,25 @@
+/* eslint-disable */
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
 import userEvent from '@testing-library/user-event';
+import App from './App';
 
 describe('Home page', () => {
   it('Header text', () => {
     render(<App />);
-    
-    const text = screen.getByText('Empresas Cadastradas')
-    expect(text).toBeTruthy()
+
+    const text = screen.getByText('Empresas Cadastradas');
+    expect(text).toBeTruthy();
   });
   it('add a new company', () => {
-    render(<App />)
-    
-    const input = screen.getByPlaceholderText('Nome da empresa')
-    const button = screen.getByText('Adicionar empresa')
-    
-    userEvent.type(input, 'Dock Tech')
-    userEvent.click(button)
-    
-    expect(screen.getByText('Dock Tech')).toBeTruthy()
-  })
-})
+    render(<App />);
+
+    const input = screen.getByPlaceholderText('Nome da empresa');
+    const button = screen.getByText('Adicionar empresa');
+
+    userEvent.type(input, 'Dock Tech');
+    userEvent.click(button);
+
+    expect(screen.getByText('Dock Tech')).toBeTruthy();
+  });
+});
